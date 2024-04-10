c7dadb6ebd12ece8654f03d9bb2c33024731928a
lily1029 <lily.zhao1029@gmail.com>
2024-04-10 12:38:37 -0700

    Updated home page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('furniture/', views.furniture_index, name="index"),
+    # path('furniture/', views.furniture_index, name="index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
+def home(request):
+    return render(request, 'home.html')
 
-# Create your views here.
-def furniture_index(request):
 
-	# tell the model to find all the rows in the cats table!
-	furniture = Furniture.objects.all()
-	return render(request, 'furniture/index.html', {
-		'furniture': furniture
-		# 'furniture' becomes a variable name in 'furniture/index.html'
-		# just like express
-		# res.render('furniture/index', {'furniture': furniture})
-	})
