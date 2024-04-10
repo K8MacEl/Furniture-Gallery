e881dfa047f5aba1a59a69368c8584af77b47454
JJ Ratzlaff <jjratzlaff@gmail.com>
2024-04-10 12:07:09 -0700

    login/signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('furniture/', views.furniture_index, name="index"),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
