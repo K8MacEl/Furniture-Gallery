83c9f82fe80ebf535488357a9244d39011256d39
k8mcelhnaey <kqmcelhaney@gmail.com>
2024-04-11 19:27:27 -0600

    removed materialized

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Furniture_Item
+from .models import Furniture_Item, Photo
 
 admin.site.register(Furniture_Item)
+admin.site.register(Photo)
